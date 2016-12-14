d94def8490ba935e4f6b4925bd197bcd61d62f12
Mike Schmid <mike.schmid@namics.com>
2016-12-14 16:06:27 +0100

    add solutions for day 13

---
+++ src/test/java/adventofcode/day13/Day13PuzzleTest.java
@@ -0,0 +1,22 @@
+package adventofcode.day13;
+
+import junit.framework.Assert;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+
+/**
+ * @author Mike Schmid
+ */
+public class Day13PuzzleTest {
+
+    @Test
+    public void solve() throws Exception {
+        Assert.assertEquals(11, Day13Puzzle.solve(Pair.of(1, 1), Pair.of(7, 4), 10));
+    }
+
+    @Test
+    public void solveReal() throws Exception {
+        Assert.assertEquals(92, Day13Puzzle.solve(Pair.of(1, 1), Pair.of(31, 39), 1350));
+    }
+
+}
